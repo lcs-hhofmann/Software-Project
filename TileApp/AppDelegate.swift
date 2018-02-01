978c0d51277ccd2237bdd4c7af1d0a38624a86b3
Hannah Hofmann <hhofmann@lcs.on.ca>
2018-02-01 14:48:27 -0500

    Initial Commit

---
+++ TileApp/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  TileApp
+//
+//  Created by Hofmann, Hannah on 2018-02-01.
+//  Copyright © 2018 Hofmann, Hannah. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
